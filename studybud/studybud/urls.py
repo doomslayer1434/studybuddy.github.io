0c6fd14e7ac87b85c4c7c309431e196c9a8a645e
doomslayer1434 <117589442+doomslayer1434@users.noreply.github.com>
2022-11-06 22:12:26 +0530

    studbud

--- studybud/studybud/urls.py
+++
@@ -1,8 +0,0 @@
-"""studybud URL Configuration"""
-from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('',include('base.urls')),
-]
